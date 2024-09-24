bd9870831d0692cf2f7edeb0a629a1fc3d09bd20
234832 <234832@KB37-309-C15>
2024-09-24 10:29:37 +0300

    Добавьте файлы проекта.

---
+++ PR3/PR3.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Сложение 1" << endl;
+    cout << "Вычитание 2" << endl;
+    cout << "Умножение 3" << endl;
+    cout << "Деление 4" << endl;
+    cout << "остаток от деления  5" << endl;
+    int a;
+    int b, c;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    if (a == 1)
+    {
+        cout << (b + c) << endl;
+    }
+    if (a == 3)
+    {
+        cout << (b * c) << endl;
+    }
+    if (a == 4)
+    {
+        cout << (b / c) << endl;
+        cout <<"Остаток от деления =" << (b % c) << endl;
+    }
+    if (a == 2 and b >= c)
+    {
+        cout << (b - c) << endl;
+    }
+    
+    if (a == 2 and b < c)
+    {
+        cout << "-"<<(c - b) << endl;
+    }
+ }
+
